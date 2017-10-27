c4e29c3443f001c12ed988f1d155adef3d66e621
Wu Weixin <wuweixin@gmail.com>
2017-10-27 15:07:57 +0800

    first commit

---
+++ src/LaravelResourceServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace Wuwx\LaravelResource;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelResourceServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
